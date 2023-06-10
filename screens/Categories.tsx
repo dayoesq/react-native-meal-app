b4127aaa2464afcdf431dbfdcf32a9fdb456c72c
Dayo Oladapo <dayoesq@live.com>
2023-06-10 10:26:48 +0300

    Menu grid item set up

---
+++ screens/Categories.tsx
@@ -0,0 +1,28 @@
+import { FlatList } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+type ItemData = {
+    item: { title: string; color: string };
+};
+
+// Placed outside because it doesn't have to be re-created!
+function renderCategoryItem(itemData: ItemData) {
+    return (
+        <CategoryGridTile
+            title={itemData.item.title}
+            color={itemData.item.color}
+        />
+    );
+}
+
+export default function Category() {
+    return (
+        <FlatList
+            data={CATEGORIES}
+            renderItem={item => renderCategoryItem(item)}
+            keyExtractor={(item, _index) => item.id}
+            numColumns={2}
+        />
+    );
+}
